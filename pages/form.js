9fef57c7aca158dbc1029e7069542fe7a1f96199
pauleena phan <pauleena2002@gmail.com>
2023-10-22 23:00:20 -0700

    form is taking in values

---
+++ pages/form.js
@@ -0,0 +1,27 @@
+import {createToDo} from "../pages/todoFunctions.js";
+
+const subbtn = document.getElementById("submit");
+
+export function inputTodo(){
+    console.log("pressing submit");
+    createToDo(document.getElementById("title").value, document.getElementById("desc").value, document.getElementById("dueDate").value, document.getElementById("priority").value);
+
+}
+
+//when you click new task
+export function showForm(){
+   document.getElementById("form").style.visibility = 'visible';
+   
+}
+
+//after pressing submit it inputs the form and creates the todo
+subbtn.addEventListener('click', inputTodo);
+
+//hides form after pressing submit
+document.getElementById("form").onsubmit = () =>{
+    console.log("testing hidden form");
+    document.getElementById("form").style.visibility = 'hidden';
+}
+
+
+
