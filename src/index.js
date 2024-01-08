6054e0504b9f84f6ae2d1daf2954b5e625c8c3ec
pauleena phan <pauleena2002@gmail.com>
2024-01-07 19:57:41 -0800

    loaded more functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 console.log("hi");
 
 import {loadhome} from "../pages/homepage.js"
+import {loadhomeTab} from "../pages/home.js";
+import {loadTodoColumn} from "../pages/todoFunctions.js";
 
 loadhome();
+loadhomeTab();
+loadTodoColumn();
+// inputTodo();
+
+
