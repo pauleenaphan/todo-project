2290764fc526b698179e7411fce4fc9cb455337e
pauleena phan <pauleena2002@gmail.com>
2024-01-24 12:47:15 -0800

    added remove todo function

--- pages/todoFunctions.js
+++ pages/todoFunctions.js
@@ -1,5 +1,5 @@
 //creates the ToDos
-export function createToDo(title, description, duedate, priority){
+export function createTodo(title, description, duedate, priority){
     //allows multiple values for the value parameter
     var toDo = {
         title: title,
@@ -15,23 +15,22 @@ export function createToDo(title, description, duedate, priority){
 
 //Loads the todo when a new one is created
 export function loadTodoColumn(){
+    //need to empty list so when it loads again it does not load the one we removed
+    document.getElementById("toDoList").innerHTML = " ";
     for(const key of Object.keys(localStorage)){
-        // console.log(key, localStorage.getItem(key));
-        
         var storedToDo = localStorage.getItem(key);
         var aboutToDo = JSON.parse(storedToDo);
-        // console.log("using key", key)
-        // console.log("Using todo", aboutToDo.title)
 
         const toDo = document.createElement("button");
         toDo.setAttribute("id", key);
         toDo.textContent = (key);
         toDo.classList.add("toDoTask");
-        toDo.addEventListener("click", loadToDoTask)
+        toDo.addEventListener("click", loadToDoTask);
         document.getElementById("toDoList").append(toDo);
     }
 }
 
+//loads the todo information when its tab is clicked
 export function loadToDoTask(e){
     console.log("testing loadtodotask")
     const inputTitle = (e.target).innerHTML
@@ -41,13 +40,16 @@ export function loadToDoTask(e){
         var storedToDo = localStorage.getItem(key);
         var aboutToDo = JSON.parse(storedToDo);
         if(key == inputTitle){
-            // console.log("key:" + key)
-            // console.log("inputTitle" + inputTitle)
 
             //empty the div before displaying the new element
             document.getElementById("currentToDo").innerHTML = "";
 
             //displays the new todo information
+            const toDoTitle = document.createElement("id");
+            toDoTitle.setAttribute("id", "toDoTitle");
+            toDoTitle.textContent = (aboutToDo.title);
+            document.getElementById("currentToDo").append(toDoTitle);
+
             const toDoDesc = document.createElement("id");
             toDoDesc.setAttribute("id", "description");
             toDoDesc.textContent = (aboutToDo.description);
@@ -62,11 +64,25 @@ export function loadToDoTask(e){
             toDoPrio.setAttribute("id", "priority");
             toDoPrio.textContent = (aboutToDo.priority);
             document.getElementById("currentToDo").append(toDoPrio);
+
+            const xbtn = document.createElement("button");
+            xbtn.setAttribute("id", "button");
+            xbtn.textContent = ("Remove Task");
+            xbtn.addEventListener('click', removeTodo);
+            document.getElementById("currentToDo").append(xbtn);
+            
         }
     }
 }   
 
-// //deletes todo 
-// export function removeToDo(title){
-//     localStorage.removeItem
-// }
+//deletes todo 
+export function removeTodo(){
+    console.log("testing remove");
+    var removeTitle = document.getElementById("toDoTitle").innerHTML
+    console.log(removeTitle)
+    localStorage.removeItem(removeTitle)
+
+    //load the column list again so that it shows the element is removed
+    loadTodoColumn()
+    document.getElementById("currentToDo").innerHTML = " "
+}
