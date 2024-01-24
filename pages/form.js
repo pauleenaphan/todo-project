6686e1c0fad5154dd68af8ccd0c9dcb3177ff36b
pauleena phan <pauleena2002@gmail.com>
2024-01-24 12:46:26 -0800

    cleaned up file

--- pages/form.js
+++ pages/form.js
@@ -1,13 +1,14 @@
-import {createToDo} from "../pages/todoFunctions.js";
+import {createTodo, removeTodo} from "../pages/todoFunctions.js";
 
 const subbtn = document.getElementById("submit");
 
+//allows users to input their todo information
 export function inputTodo(){
     console.log("pressing submit");
-    createToDo(document.getElementById("title").value, document.getElementById("desc").value, document.getElementById("dueDate").value, document.getElementById("priority").value);
+    createTodo(document.getElementById("title").value, document.getElementById("desc").value, document.getElementById("dueDate").value, document.getElementById("priority").value);
 }
 
-//when you click new task
+//when you click new task a form will popup
 export function showForm(){
    document.getElementById("form").style.visibility = 'visible';
    
@@ -15,8 +16,5 @@ export function showForm(){
 
 //after pressing submit it inputs the form and creates the todo
 subbtn.addEventListener('click', inputTodo);
-
-
-
 
 
