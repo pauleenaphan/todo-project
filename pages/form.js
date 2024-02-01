ad3e0d96a206aefed32af33305ef25798fd04e4c
pauleena phan <pauleena2002@gmail.com>
2024-02-01 13:48:23 -0800

    cleaned up file

--- pages/form.js
+++ pages/form.js
@@ -1,4 +1,4 @@
-import {createTodo, removeTodo} from "../pages/todoFunctions.js";
+import {createTodo} from "../pages/todoFunctions.js";
 
 const subbtn = document.getElementById("submit");
 
@@ -8,12 +8,12 @@ export function inputTodo(){
     createTodo(document.getElementById("title").value, document.getElementById("desc").value, document.getElementById("dueDate").value, document.getElementById("priority").value);
 }
 
-//when you click new task a form will popup
+//when you click new task a form for a new todo will popup
 export function showForm(){
     document.getElementById("form").style.display = "block";
+
     //When we click new form we get rid of the current todo tab if the user is on one
     document.getElementById("currentToDo").innerHTML = " ";
-   
 }
 
 //after pressing submit it inputs the form and creates the todo
