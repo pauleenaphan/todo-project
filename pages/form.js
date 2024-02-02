13deb71cb960125ea958082a88700be1ca914158
pauleena phan <pauleena2002@gmail.com>
2024-02-01 19:22:27 -0800

    added loadedit form function

--- pages/form.js
+++ pages/form.js
@@ -1,6 +1,7 @@
-import {createTodo} from "../pages/todoFunctions.js";
+import {createTodo, replaceTodo} from "../pages/todoFunctions.js";
 
 const subbtn = document.getElementById("submit");
+const editsubbtn = document.getElementById("editsubmit");
 
 //allows users to input their todo information
 export function inputTodo(){
@@ -16,7 +17,30 @@ export function showForm(){
     document.getElementById("currentToDo").innerHTML = " ";
 }
 
-//after pressing submit it inputs the form and creates the todo
+//after pressing submit for the new task form it inputs the form and creates the todo
 subbtn.addEventListener('click', inputTodo);
 
 
+
+export function loadEditArea(){
+    document.getElementById("editTodoPopup").style.display = "block";
+    //first sets the textarea box to the current title
+    document.getElementById("editTitle").innerHTML = document.getElementById("toDoTitle").textContent;
+    //then sets the current title to the new inputted title in the box
+    document.getElementById("toDoTitle").textContent = document.getElementById("editTitle").innerHTML;
+
+    //description
+    document.getElementById("editDesc").innerHTML = document.getElementById("toDodescription").textContent;
+    document.getElementById("toDodescription").textContent = document.getElementById("editDesc").innerHTML;
+
+    //due date
+    document.getElementById("editDueDate").innerHTML = document.getElementById("toDodueDate").textContent;
+    document.getElementById("toDodueDate").textContent = document.getElementById("editDueDate").innerHTML;
+
+    //priority
+    document.getElementById("editPriority").innerHTML = document.getElementById("toDopriority").textContent;
+    document.getElementById("toDopriority").textContent = document.getElementById("editPriority").innerHTML;
+}
+
+//after pressing the submit edit btn, replace the old todo with a new one
+editsubbtn.addEventListener('click', replaceTodo);
