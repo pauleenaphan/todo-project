6b1cde6ddff6cfff177d95c58e5f9211da139537
pauleena phan <pauleena2002@gmail.com>
2024-03-06 22:44:56 -0800

    removed loadhomepage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 console.log("hi");
 
 import {loadhome} from "../pages/homepage.js"
-import {loadhomeTab} from "../pages/home.js";
 import {loadTodoColumn} from "../pages/todoFunctions.js";
 
 loadhome();
-loadhomeTab();
 loadTodoColumn();
 
 
