51caeef2e335ccdfd0a91910a0af40283be81218
pauleena phan <pauleena2002@gmail.com>
2024-03-07 10:19:57 -0800

    add testing and removed nothing new

--- pages/form.js
+++ pages/form.js
@@ -5,7 +5,6 @@ const editsubbtn = document.getElementById("editsubmit");
 
 //allows users to input their todo information
 export function inputTodo(){
-    console.log("pressing submit");
     createTodo(document.getElementById("title").value, document.getElementById("desc").value, document.getElementById("dueDate").value, document.getElementById("priority").value);
 }
 
